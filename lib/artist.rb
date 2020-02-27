ea2282f2aebbd867e81d9555731d7ec6e77c537d
Luis F Tavarez Valdez <luis.tavarez1995@yahoo.com>
2020-02-27 01:39:14 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -22,4 +22,16 @@ class Artist
     song.artist = self
   end
   
+  def self.find_or_create_by_name(name)
+  self.find_by_name(name) || self.create_by_name(name)
+  end
+  
+  def self.find_by_name(name)
+    @@all.find {|artist| artist.name == name}
+  end
+  
+  def self.create_by_name(name)
+    new_artist = Artist.new()
+  end
+  
 end
