86de987dd69b86cd2d08b73d9e1b6a4d7e0ae212
Luis F Tavarez Valdez <luis.tavarez1995@yahoo.com>
2020-02-27 01:52:50 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -11,6 +11,13 @@ class Song
   def self.new_by_filename(file)
     name = file.split(" - ")
     new_song = Song.new(name[1])
+    new_song.artist_name = name[0]
+    new_song
+  end
+  
+  def artist_name=(name)
+    new_artist = Artist.find_or_create_by_name(name)
+    self.artist = new_artist
   end
   
   def self.all
